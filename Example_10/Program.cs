8c851b8b6d8b29cf03ac154367acb213a135999c
Pavel <kwadrakilla666@gmail.com>
2023-01-18 17:21:16 +0300

    Add Example_10

---
+++ Example_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа
+
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int num1 = number % 100;
+int result = num1 / 10;
+
+Console.WriteLine($"Вторая цифра числа {result}");
+
+
+
