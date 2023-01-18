fd25b801b5c26c2872a3211d48a93bb9167fe0de
Pavel <kwadrakilla666@gmail.com>
2023-01-18 17:21:34 +0300

    Add Example_13

---
+++ Example_13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет. (Цифры считать справа налево)
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int num1 = number % 1000;
+int result = num1 / 100;
+if(result == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else
+{
+    Console.WriteLine($"Третья цифра {result}");
+}
+
+
+
+
+
