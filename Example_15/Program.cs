2630263a9aa6af726d7f06e3e961a232116f26b8
Pavel <kwadrakilla666@gmail.com>
2023-01-18 17:22:25 +0300

    Add Example_15

---
+++ Example_15/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите чисто от 1 до 7: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if(number == 1)
+{
+    Console.WriteLine("Понедельник (будний день)");
+}
+if(number == 2)
+{
+    Console.WriteLine("Вторник (будний день)");
+}
+if(number == 3)
+{
+    Console.WriteLine("Среда (будний день)");
+}
+if(number == 4)
+{
+    Console.WriteLine("Четверг (будний день)");
+}
+if(number == 5)
+{
+    Console.WriteLine("Пятница (будний день)");
+}
+if(number == 6)
+{
+    Console.WriteLine("Поздравляем, сегодня суббота - выходной!");
+}
+if(number == 7)
+{
+    Console.WriteLine("Поздравляем, сегодня воскресенье - выходной!");
+}
+else if(number < 1 || number > 7)
+{ 
+    Console.Write("Такого дня недели нет");
+}  
+
